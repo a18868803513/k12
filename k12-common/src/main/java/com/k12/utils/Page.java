aabf48334b420ce6f1a5cfdac44c2397d9c5eade
a18868803513 <2477149597@qq.com>
2017-09-02 16:12:38 +0800

    更新工具类

---
+++ k12-common/src/main/java/com/k12/utils/Page.java
@@ -0,0 +1,56 @@
+package com.k12.utils;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/9/2/002.
+ */
+public class Page {
+    private int ii;
+    private int pageSize; //一页的数目
+    private List<?> rows;//一页的内容
+    private int page;//当前页
+    private int total;//总数
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public List<?> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<?> rows) {
+        this.rows = rows;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "TT{" +
+                "pageSize=" + pageSize +
+                ", rows=" + rows +
+                ", page=" + page +
+                ", total=" + total +
+                '}';
+    }
+}
--- k12-common/src/main/java/com/k12/utils/TT.java
+++
@@ -1,7 +0,0 @@
-package com.k12.utils;
-
-/**
- * Created by Administrator on 2017/9/2/002.
- */
-public class TT {
-}
