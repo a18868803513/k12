25e5a851d569093979306ea19c14ad44ac5faced
a18868803513 <2477149597@qq.com>
2017-09-12 11:23:28 +0800

    更新未登录拦截，加一个爬虫jar包

--- k12-manager/k12-manager-dao/src/test/java/com/k12/text/MyTest.java
+++ k12-manager/k12-manager-dao/src/test/java/com/k12/text/MyTest.java
@@ -1,3 +1,4 @@
+/*
 package com.k12.text;
 
 import com.k12.utils.Article;
@@ -10,33 +11,49 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+*/
 /**
  * Created by Administrator on 2017/9/2/002.
- */
+ *//*
+
 public class MyTest {
     public static void main(String args[]) throws IOException {
         String startUrl = "http://read.qidian.com/chapter/ia-u3DXBy9B7DVpbqm07HA2/TuCGfeMZgTJMs5iq0oQwLQ2";
-        /*Document doc = Jsoup.parseBodyFragment(startUrl);
-        System.out.println(doc);*/
-       /* Document doc=Jsoup.parse(startUrl);
+        */
+/*Document doc = Jsoup.parseBodyFragment(startUrl);
+        System.out.println(doc);*//*
+
+       */
+/* Document doc=Jsoup.parse(startUrl);
         Element body=doc.body();
-        System.out.println(body);*/
+        System.out.println(body);*//*
+
+*/
 /*        Document doc = Jsoup.connect("http://www.bxwx.org/b/5/5131/832883.html").data("query", "Java")
                 .userAgent("Mozilla")
                 .cookie("auth", "token")
                 .timeout(3000)
-                .post();*/
+                .post();*//*
+
         Document doc = Jsoup.connect("http://read.qidian.com/chapter/ia-u3DXBy9B7DVpbqm07HA2/TuCGfeMZgTJMs5iq0oQwLQ2").get();
         Document doc1=Jsoup.connect("http://www.youku.com/").get();
         System.out.println(doc1);
-       /* System.out.println("============标题===="+doc.title());
-        System.out.println(doc.select("a#j_chapterPrev").attr("href"));*/
-       /* System.out.println(doc);*/
+       */
+/* System.out.println("============标题===="+doc.title());
+        System.out.println(doc.select("a#j_chapterPrev").attr("href"));*//*
 
-       /* System.out.println( doc.getElementsByClass("read-content j_readContent"));*/
- /*     String title = doc.title();
+       */
+/* System.out.println(doc);*//*
+
+
+       */
+/* System.out.println( doc.getElementsByClass("read-content j_readContent"));*//*
+
+ */
+/*     String title = doc.title();
         System.out.println(title);
-        System.out.println(doc.getElementById("content"));*/
+        System.out.println(doc.getElementById("content"));*//*
+
     }
     @Test
     public  void text1() throws IOException {
@@ -75,20 +92,24 @@ public class MyTest {
         }
         return doc;
     }
-    /**
+    */
+/**
      * 根据获取的Document对象找到章节标题
      * @param doc
      * @return 标题
-     */
+     *//*
+
     public static String getTitle(Document doc){
         return doc.getElementById("newstitle").val();
     }
 
-    /**
+    */
+/**
      * 根据获取的Document对象找到小说内容
      * @param doc
      * @return 内容
-     */
+     *//*
+
     public static String getContent(Document doc){
         if(doc.getElementById("left_zw") != null){
             return doc.getElementById("left_zw").text();
@@ -97,11 +118,13 @@ public class MyTest {
         }
 
     }
-    /**
+    */
+/**
      * 根据获取的Document对象找到下一章的Url地址
      * @param doc
      * @return 下一章Url
-     */
+     *//*
+
     public static String getNextUrl(Document doc){
         Element ul = doc.select("ul").first();
         String regex = "<li><a href=\"(.*?)\">下一页<\\/a><\\/li>";
@@ -119,21 +142,25 @@ public class MyTest {
 
     }
 
-    /**
+    */
+/**
      * 根据url获取id
      * @param url
      * @return id
-     */
+     *//*
+
     public static String getId(String url){
         String urlSpilts[] = url.split("/");
         return (urlSpilts[urlSpilts.length - 1]).split("\\.")[0];
     }
 
-    /**
+    */
+/**
      * 根据小说的Url获取一个Article对象
      * @param url
      * @return
-     */
+     *//*
+
     public static Article getArticle(String url){
         Article article = new Article();
         article.setUrl(url);
@@ -145,3 +172,4 @@ public class MyTest {
         return article;
     }
 }
+*/
