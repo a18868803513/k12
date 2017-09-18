110e0ab2c5e7ab79ba773dce332cb2e1ee9f28b8
Dongwenyong <425238061@qq.com>
2017-09-18 19:37:13 +0800

    支付功能

---
+++ k12-manager/k12-manager-web/src/main/java/com/k12/web/PayController.java
@@ -0,0 +1,72 @@
+package com.k12.web;
+
+import com.k12.service.OrderService;
+import com.k12.utils.PaymentUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ResourceBundle;
+
+/**
+ * Created by dwy on 2017/9/18.
+ */
+@Controller
+public class PayController {
+    @Autowired
+    OrderService orderService;
+
+    @RequestMapping("payLast")
+    public String payLast(HttpServletRequest request,HttpServletResponse response){
+        // 获得 支付必须基本数据
+        String orderid = request.getParameter("orderid");
+        String money = request.getParameter("money");
+        // 银行
+        String pd_FrpId = request.getParameter("pd_FrpId");
+
+        // 发给支付公司需要哪些数据
+        String p0_Cmd = "Buy";
+        String p1_MerId = ResourceBundle.getBundle("merchantInfo").getString(
+                "p1_MerId");
+        String p2_Order = orderid;
+        String p3_Amt = money;
+        String p4_Cur = "CNY";
+        String p5_Pid = "";
+        String p6_Pcat = "";
+        String p7_Pdesc = "";
+        // 支付成功回调地址 ---- 第三方支付公司会访问、用户访问
+        // 第三方支付可以访问网址
+        String p8_Url = "http://10.31.152.16:8080/bookStore/CallbackServlet";
+        String p9_SAF = "";
+        String pa_MP = "";
+        String pr_NeedResponse = "1";
+        // 加密hmac 需要密钥
+        String keyValue = ResourceBundle.getBundle("merchantInfo").getString(
+                "keyValue");
+        String hmac = PaymentUtil.buildHmac(p0_Cmd, p1_MerId, p2_Order, p3_Amt,
+                p4_Cur, p5_Pid, p6_Pcat, p7_Pdesc, p8_Url, p9_SAF, pa_MP,
+                pd_FrpId, pr_NeedResponse, keyValue);
+        // 生成url --- url?
+        request.setAttribute("pd_FrpId", pd_FrpId);
+        request.setAttribute("p0_Cmd", p0_Cmd);
+        request.setAttribute("p1_MerId", p1_MerId);
+        request.setAttribute("p2_Order", p2_Order);
+        request.setAttribute("p3_Amt", p3_Amt);
+        request.setAttribute("p4_Cur", p4_Cur);
+        request.setAttribute("p5_Pid", p5_Pid);
+        request.setAttribute("p6_Pcat", p6_Pcat);
+        request.setAttribute("p7_Pdesc", p7_Pdesc);
+        request.setAttribute("p8_Url", p8_Url);
+        request.setAttribute("p9_SAF", p9_SAF);
+        request.setAttribute("pa_MP", pa_MP);
+        request.setAttribute("pr_NeedResponse", pr_NeedResponse);
+        request.setAttribute("hmac", hmac);
+
+    return "confirm";
+
+    }
+
+
+}
